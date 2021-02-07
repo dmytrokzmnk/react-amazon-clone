536a2f98315efce5a8a3b76b33c38186a7952b35
Dmytro Kuzmenko <dmytrokzmnk@gmail.com>
2021-02-08 01:54:11 +0200

    created auth logic firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: API_KEY,
+  apiKey: "AIzaSyA_1JyiImR8qP4YRVhO23YDcgAT2RzxeOI",
   authDomain: "e-clone-eb1d1.firebaseapp.com",
   projectId: "e-clone-eb1d1",
   storageBucket: "e-clone-eb1d1.appspot.com",
@@ -9,3 +9,10 @@ const firebaseConfig = {
   appId: "1:509113267080:web:af02f6e275f90548cbc03d",
   measurementId: "G-JTSM5ZJ884",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
