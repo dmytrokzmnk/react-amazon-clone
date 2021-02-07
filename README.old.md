9264a99c903d929b783a80f9f294ddc4ca7c60b0
Dmytro Kuzmenko <dmytrokzmnk@gmail.com>
2021-02-07 16:44:02 +0200

    added firebase config

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# react-amazon-clone
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: "e-clone-eb1d1.firebaseapp.com",
+  projectId: "e-clone-eb1d1",
+  storageBucket: "e-clone-eb1d1.appspot.com",
+  messagingSenderId: "509113267080",
+  appId: "1:509113267080:web:af02f6e275f90548cbc03d",
+  measurementId: "G-JTSM5ZJ884",
+};
